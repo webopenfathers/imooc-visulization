4b9f411cd6138a940d4eb1120773c35ce1371326
webopenfathers <1290443925@qq.com>
2023-09-30 10:01:03 +0800

    2023 9-30 10:00 绘制关键词条文档云图

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>【服务资源占用比】</div>
+    <div>【关键词条】</div>
     <div ref="target" class="w-full h-full"></div>
   </div>
 </template>
@@ -8,6 +8,7 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue'
 import * as echarts from 'echarts'
+import 'echarts-wordcloud'
 
 const props = defineProps({
   data: {
@@ -23,8 +24,53 @@ onMounted(() => {
   renderChart()
 })
 
+const randomRGB = () => {
+  const r = Math.floor(Math.random() * 255)
+  const g = Math.floor(Math.random() * 255)
+  const b = Math.floor(Math.random() * 255)
+
+  return `rgb(${r},${g},${b})`
+}
+
 const renderChart = () => {
-  const options = {}
+  const options = {
+    tooltip: {
+      show: true,
+      borderColor: '#fe9a8bb3',
+      borderWidth: 1,
+      padding: [10, 15, 10, 15],
+      confine: true,
+      backgroundColor: 'rgba(255, 255, 255, .9)',
+      textStyle: {
+        color: 'hotpink',
+        lineHeight: 22,
+      },
+      extraCssText:
+        'box-shadow: 0 4px 20px -4px rgba(199, 206, 215, .7);border-radius: 4px;',
+    },
+    series: [
+      {
+        type: 'wordCloud',
+        // 设置文字大小范围
+        sizeRange: [8, 46],
+        // 设置文字旋转
+        rotationRange: [0, 0],
+        // 单词之间的间距
+        gridSize: 0,
+        // 渲染动画
+        layoutAnimation: true,
+        // 字体样式
+        textStyle: {
+          // 随机色值
+          color: randomRGB,
+        },
+        emphasis: {
+          focus: 'none',
+        },
+        data: props.data.datas,
+      },
+    ],
+  }
 
   myChart.setOption(options)
 }
